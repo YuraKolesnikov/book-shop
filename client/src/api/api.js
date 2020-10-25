1456ea333828492e484064d08d7faf574a8f5408
Yurii Kolesnikov <yk@vcv.ru>
2020-10-25 17:55:23 +0700

    Added qs module for params serializing

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,20 +1,26 @@
-const { Axios } = require('./axios')
-console.log('Axios loaded')
+import qs from 'qs'
+import { Axios } from './axios'
+
 export const api = (url, queryParams = null) => {
+  const config = {
+    params: queryParams,
+    paramsSerializer: params => qs.stringify(params)
+  }
+
   return {
-    post: data => Axios.post(url, data)
+    post: data => Axios.post(url, data, config)
       .then(res => res)
       .catch(e => console.error(e)),
 
-    get: () => Axios.get(url)
+    get: () => Axios.get(url, config)
       .then(res => res)
       .catch(e => console.error(e)),
 
-    put: data => Axios.put(url, data)
+    put: data => Axios.put(url, data, config)
       .then(res => res)
       .catch(e => console.error(e)),
 
-    delete: () => Axios.delete(url)
+    delete: () => Axios.delete(url, config)
       .then(res => res)
       .catch(e => console.error(e))
   }
