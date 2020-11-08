bfb5c8f02f62bbf86bb78b5e4315cfe41c2a62ca
Yurii Kolesnikov <yk@vcv.ru>
2020-11-08 15:28:49 +0700

    Added some helpers

--- client/src/helpers/object.js
+++ client/src/helpers/object.js
@@ -1,31 +1,26 @@
-import { getTypeOf } from '@/helpers/data';
+import { getTypeOf } from '@/helpers/typeof';
 
-function clearField(obj, field) {
-	const type = getTypeOf(obj[field])
+export const clearObject = (obj, mode = 'soft') => {
+	Object.keys(obj).forEach(field => {
+		const type = getTypeOf(obj[field])
 
-	if (type === 'null' || type === 'undefined') {
-		obj[field] = null
-	} else if (type === 'map') {
-		obj[field] = new Map()
-	} else if (type === 'set') {
-		obj[field] = new Set()
-	} else if (type === 'object') {
-		clearObject(obj[field])
-	} else if (type === 'number') {
-		obj[field] = 0
-	} else if (type === 'boolean') {
-		obj[field] = false
-	} else {
-		obj[field] = ''
-	}
-}
-
-export const clearObject = obj => {
-	Object.keys(obj).forEach(key => clearField(obj, key))
-}
-
-export const clearObjectField = (obj, field) => clearField(obj, field)
-
-export const findInObject = ({ obj, key, val }) => {
-	console.log(obj, { key, val })
+		if (type === 'map') {
+			obj[field] = new Map()
+		} else if (type === 'set') {
+			obj[field] = new Set()
+		} else if (type === 'object') {
+			if (mode === 'soft') {
+				clearObject(obj[field])
+			} else
+				obj[field] = {}
+		} else if (type === 'array') {
+			obj[field] = []
+		} else if (type === 'number') {
+			obj[field] = 0
+		} else if (type === 'boolean') {
+			obj[field] = false
+		} else {
+			obj[field] = null
+		}
+	})
 }
